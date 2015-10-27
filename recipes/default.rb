d32221cb157654212e74b8c9cf43e1893bc99e39
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-27 22:43:46 +0100

    Default recipe for installing rvm

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,13 +9,24 @@
 
 home = node['rvm']['user']['dir']
 
-file 'lockrvm' do
+package %w(gnupg curl)
+
+file "#{home}/.lockrvm" do
   action :create_if_missing
-  name "#{home}/.lockrvm"
-  notifies :run, 'execute[initrvm]', :immediately
+  name 'lock_rvm'
+  notifies :run, 'ruby_block[init_rvm]', :immediately
 end
 
-execute 'initrvm' do
-  command 'curl -sSL https://get.rvm.io | bash -s stable'
-  action :nothing
+ruby_block 'init_rvm' do
+  block do
+    cmd = Mixlib::ShellOut.new(
+      'gpg --keyserver hkp://keys.gnupg.net ' \
+          '--recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3')
+    cmd.run_command
+    cmd.error!
+    cmd = Mixlib::ShellOut.new('\curl -sSL https://get.rvm.io | bash -s stable')
+    cmd.run_command
+    cmd.error!
+  end
+  action :create
 end

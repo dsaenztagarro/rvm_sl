7ace4144aafdbe64aa13804c6eb3f783a5ee8dcf
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-03 00:45:58 +0100

    Added tests on notifications

--- spec/user_install_spec.rb
+++ spec/user_install_spec.rb
@@ -5,12 +5,19 @@ describe 'rvm::user_install' do
 
   let(:chef_run) do
     ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04') do |node|
-      node.set['rvm']['user']['dir'] = '/home/vagrant'
+      node.set['rvm']['user']['name'] = 'vagrant'
+      node.set['rvm']['user']['password'] = 'vagrant'
     end.converge described_recipe
   end
 
-  it 'creates a lock file on install' do
-    expect(chef_run).to create_file("#{home}/.lockrvm")
+  it 'sends a notification to bootstrap bashrc' do
+    ruby_block = chef_run.ruby_block('install_rvm')
+    expect(ruby_block).to notify('execute[bootstrap_bashrc]').to(:run).immediately
+  end
+
+  it 'sends a notification to create lock file' do
+    bootstrap_command = chef_run.execute('bootstrap_bashrc')
+    expect(bootstrap_command).to notify('file[lock_rvm]').to(:create).immediately
   end
 
   it 'converges successfully' do

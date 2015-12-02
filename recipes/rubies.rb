8b1ba705083d1044b81980098230c22d4e46857f
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-02 10:59:30 +0100

    Updates recipe header comment

--- recipes/rubies.rb
+++ recipes/rubies.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: rvm
+# Cookbook Name:: rvm_sl
 # Recipe:: rubies
 #
 # Copyright 2015, David Saenz Tagarro
--- recipes/user_install.rb
+++ recipes/user_install.rb
@@ -1,6 +1,6 @@
 #
-# Cookbook Name:: rvm
-# Recipe:: default
+# Cookbook Name:: rvm_sl
+# Recipe:: user_install
 #
 # Copyright 2015, David Saenz Tagarro
 #
